184618ab6e4e3307ac486d08a95c3039eba0e966
Jean Paul <jeanpaullm>
2017-05-27 15:38:07 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/HADB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HADB.dir/main.cpp.o"
+  "HADB.pdb"
+  "HADB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HADB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HADB.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HADB.
+# This may be replaced when dependencies are built.
